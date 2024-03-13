27cf22dfca6d44be6b1a9fa6915d469a49b07a78
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2024-03-13 21:45:49 +0700

    add input test

--- src/boss_babys_revenge.rs
+++ src/boss_babys_revenge.rs
@@ -1,5 +1,6 @@
 pub const MAX_INPUT_LEN: usize = 1000000;
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Input(Vec<Action>);
 
 #[derive(Debug, PartialEq, Eq)]
@@ -9,10 +10,10 @@ pub enum InputConversionError {
     InvalidCharacter,
 }
 
-impl TryFrom<String> for Input {
+impl TryFrom<&str> for Input {
     type Error = InputConversionError;
 
-    fn try_from(value: String) -> Result<Self, Self::Error> {
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
         use InputConversionError::*;
         if value.is_empty() {
             return Err(EmptyString);
@@ -47,40 +48,57 @@ pub enum Output {
     BadBoy,
 }
 
-pub fn check_boss_behavior(actions: impl IntoIterator<Item = Action>) -> Output {
+pub fn check_boss_behavior(input: Input) -> Output {
     todo!()
 }
 
 #[cfg(test)]
 mod test {
+    use std::iter;
+
     use super::*;
     use Action::*;
+    use InputConversionError::*;
+    use Output::*;
+
+    #[test]
+    fn validate_input_over_max() {
+        let string = "S".repeat(MAX_INPUT_LEN + 1);
+        assert_eq!(Input::try_from(string.as_str()), Err(StringOverMaxLength))
+    }
+
+    #[test]
+    fn validate_input_empty() {
+        assert_eq!(Input::try_from(""), Err(EmptyString))
+    }
+
+    #[test]
+    fn validate_input_invalid_character() {
+        assert_eq!(Input::try_from("A"), Err(InvalidCharacter))
+    }
 
     #[test]
     fn pos_1() {
-        assert_eq!(check_boss_behavior([S, R, S, S, R, R, R]), Output::GoodBoy);
+        assert_eq!(check_boss_behavior("SRSSRRR".try_into().unwrap()), GoodBoy);
     }
 
     #[test]
     fn pos_2() {
-        assert_eq!(check_boss_behavior([S, S, R, R]), Output::GoodBoy);
+        assert_eq!(check_boss_behavior("SSRR".try_into().unwrap()), GoodBoy);
     }
 
     #[test]
     fn neg_1() {
-        assert_eq!(check_boss_behavior([R, S, S, R, R]), Output::BadBoy);
+        assert_eq!(check_boss_behavior("RSSRR".try_into().unwrap()), BadBoy);
     }
 
     #[test]
     fn neg_2() {
-        assert_eq!(
-            check_boss_behavior([S, S, S, R, R, R, R, S]),
-            Output::BadBoy
-        );
+        assert_eq!(check_boss_behavior("SSSRRRRS".try_into().unwrap()), BadBoy);
     }
 
     #[test]
     fn neg_3() {
-        assert_eq!(check_boss_behavior([S, R, R, S, S, R]), Output::BadBoy);
+        assert_eq!(check_boss_behavior("SRRSSR".try_into().unwrap()), BadBoy);
     }
 }
