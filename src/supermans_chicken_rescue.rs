73c778d639c50c2d3b79e7784f4ce1cbf1e5df6e
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2024-03-14 08:01:52 +0700

    implement max_chicken_protected, add range type for input validation

--- src/supermans_chicken_rescue.rs
+++ src/supermans_chicken_rescue.rs
@@ -1,3 +1,5 @@
+use std::{ops::Deref, sync::Arc, thread};
+
 use itertools::{Itertools, Unique};
 
 const MAX_CHICKEN_NUM_LENGTH: usize = 1000000;
@@ -5,47 +7,67 @@ const MAX_ROOF_LENGTH: usize = 1000000;
 const MAX_CHICKEN_POSITION: u32 = 1000000000;
 
 #[derive(Debug, PartialEq, Eq)]
+pub enum RangeError {
+    UnderRange,
+    OverRange,
+}
+
+// I want to validate input by type
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct LimitedInputRange<const MIN: usize, const MAX: usize>(usize);
+
+impl<const MIN: usize, const MAX: usize> TryFrom<usize> for LimitedInputRange<MIN, MAX> {
+    type Error = RangeError;
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        use RangeError::*;
+        match value {
+            under if under < MIN => Err(UnderRange),
+            over if over > MAX => Err(OverRange),
+            _ => Ok(LimitedInputRange(value)),
+        }
+    }
+}
+
+// for ease of use
+impl<const MIN: usize, const MAX: usize> Deref for LimitedInputRange<MIN, MAX> {
+    type Target = usize;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub type ChickenNum = LimitedInputRange<1, MAX_CHICKEN_NUM_LENGTH>;
+pub type RoofLength = LimitedInputRange<1, MAX_ROOF_LENGTH>;
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum InputError {
     ChickenNumNotMatch,
     ChickenNumOutOfRange,
     RoofLengthOutOfRange,
     ChickenPositionOutOfRange,
-    ChickenPositionLengthOutOfRange,
     ChickenPositionNotUnique,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Input {
-    chicken_num: usize,
-    roof_length: usize,
+    chicken_num: ChickenNum,
+    roof_length: RoofLength,
     chicken_positions: Vec<u32>,
 }
 
 impl Input {
     pub fn new(
-        chicken_num: usize,
-        roof_length: usize,
+        chicken_num: ChickenNum,
+        roof_length: RoofLength,
         chicken_positions: Vec<u32>,
     ) -> Result<Self, InputError> {
         use InputError::*;
 
         // O(1)
-        if chicken_num != chicken_positions.len() {
+        if *chicken_num != chicken_positions.len() {
             return Err(ChickenNumNotMatch);
-        }
-
-        // O(1)
-        // I assume this because range bounds are known at compile time and should have nothing to do with the elements in between
-        let is_chicken_num_in_range = (1..=MAX_CHICKEN_NUM_LENGTH).contains(&chicken_num);
-        if !is_chicken_num_in_range {
-            return Err(ChickenNumOutOfRange);
-        }
-
-        // O(1)
-        // I assume this because range bounds are known at compile time and should have nothing to do with the elements in between
-        let is_roof_length_in_range = (1..=MAX_ROOF_LENGTH).contains(&roof_length);
-        if !is_roof_length_in_range {
-            return Err(RoofLengthOutOfRange);
         }
 
         // O(N)
@@ -65,6 +87,8 @@ impl Input {
             return Err(ChickenPositionNotUnique);
         }
 
+        // will not validate the position order, assuming from the specs that it's guaranteed to be sorted by ascending
+
         let new_input = Input {
             chicken_num,
             roof_length,
@@ -75,7 +99,40 @@ impl Input {
 }
 
 pub fn max_chicken_protected(input: Input) -> usize {
-    todo!()
+    // O(N)
+    // the strategy is to brute-force roof test on every chicken positions, but parallelize across threads
+
+    // wrap with arc to share across the threads
+    // since I don't need mutation, I don't need lock
+    let shared_positions = Arc::new(input.chicken_positions);
+
+    let mut join_handles = vec![];
+    for (i, p) in shared_positions.iter().enumerate() {
+        let move_positions = shared_positions.clone(); // clone before move to closure
+        let handle = thread::spawn(move || {
+            // I assume skip shouldn't take O(N) because the underlying type is vector
+            // It should be able to jump ahead in O(1)
+            let mut positions = move_positions.iter().skip(i).take(*input.roof_length);
+
+            let first = positions.next().unwrap();
+            let mut count = 1; // include first
+
+            // O(N) where N is roof_length
+            // since we already called take(roof_length), it should act as a guard
+            // take_while will not take more than roof_length
+            count += positions
+                .take_while(|&p| p - first < *input.roof_length as u32) // continue to take while the roof covers
+                .count();
+
+            count
+        });
+        join_handles.push(handle);
+    }
+
+    // I don't know if there's a better way to join multiple threads, so just linear loop for now
+    let results = join_handles.into_iter().map(|h| h.join().unwrap());
+
+    results.max().unwrap() // unwrap because previous input validation ensures that the elements are not empty
 }
 
 #[cfg(test)]
@@ -83,59 +140,110 @@ mod test {
     use super::*;
     use InputError::*;
 
-    #[test]
-    fn test_chicken_num_not_match() {
-        assert_eq!(Input::new(1, 1, vec![]), Err(ChickenNumNotMatch));
+    mod input_validation {
+        use super::*;
+        use InputError::*;
+
+        #[test]
+        fn test_chicken_num_not_match() {
+            assert_eq!(
+                Input::new(1.try_into().unwrap(), 1.try_into().unwrap(), vec![]),
+                Err(ChickenNumNotMatch)
+            );
+        }
+
+        #[test]
+        fn test_chicken_num_out_of_range() {
+            assert_eq!(ChickenNum::try_from(0), Err(RangeError::UnderRange));
+            assert_eq!(
+                ChickenNum::try_from(MAX_CHICKEN_NUM_LENGTH + 1),
+                Err(RangeError::OverRange)
+            )
+        }
+
+        #[test]
+        fn test_roof_length_out_of_range() {
+            assert_eq!(RoofLength::try_from(0), Err(RangeError::UnderRange));
+            assert_eq!(
+                RoofLength::try_from(MAX_ROOF_LENGTH + 1),
+                Err(RangeError::OverRange)
+            );
+        }
+
+        #[test]
+        fn test_chicken_position_out_of_range() {
+            assert_eq!(
+                Input::new(
+                    2.try_into().unwrap(),
+                    2.try_into().unwrap(),
+                    vec![0, MAX_CHICKEN_POSITION + 1]
+                ),
+                Err(ChickenPositionOutOfRange)
+            );
+        }
+
+        #[test]
+        fn test_chicken_position_not_unique() {
+            assert_eq!(
+                Input::new(3.try_into().unwrap(), 2.try_into().unwrap(), vec![1, 2, 2]),
+                Err(ChickenPositionNotUnique)
+            );
+        }
+
+        #[test]
+        fn test_chicken_position_length_out_of_range() {
+            assert_eq!(
+                Input::new(
+                    2.try_into().unwrap(),
+                    2.try_into().unwrap(),
+                    vec![0, (MAX_CHICKEN_POSITION + 1)]
+                ),
+                Err(ChickenPositionOutOfRange)
+            );
+        }
     }
 
     #[test]
-    fn test_chicken_num_out_of_range() {
-        let chicken_num = MAX_CHICKEN_NUM_LENGTH + 1;
-        let chicken_positions = (1..=chicken_num as u32).collect::<Vec<u32>>();
-        assert_eq!(
-            Input::new(chicken_num, 0, chicken_positions),
-            Err(ChickenNumOutOfRange)
-        );
-    }
-
-    #[test]
-    fn test_roof_length_out_of_range() {
-        assert_eq!(Input::new(1, 0, vec![1]), Err(RoofLengthOutOfRange));
-    }
-
-    #[test]
-    fn test_chicken_position_out_of_range() {
-        assert_eq!(
-            Input::new(2, 2, vec![0, MAX_CHICKEN_POSITION + 1]),
-            Err(ChickenPositionOutOfRange)
-        );
-    }
-
-    #[test]
-    fn test_chicken_position_not_unique() {
-        assert_eq!(
-            Input::new(3, 2, vec![1, 2, 2]),
-            Err(ChickenPositionNotUnique)
-        );
-    }
-
-    #[test]
-    fn test_chicken_position_length_out_of_range() {
-        assert_eq!(
-            Input::new(2, 2, vec![0, (MAX_CHICKEN_POSITION + 1)]),
-            Err(ChickenPositionOutOfRange)
-        );
-    }
-
-    #[test]
-    fn test_positive_1() {
-        let input = Input::new(5, 5, vec![2, 5, 10, 12, 15]).unwrap();
+    fn test_1() {
+        let input = Input::new(
+            5.try_into().unwrap(),
+            5.try_into().unwrap(),
+            vec![2, 5, 10, 12, 15],
+        )
+        .unwrap();
         assert_eq!(max_chicken_protected(input), 2);
     }
 
     #[test]
-    fn test_negative_1() {
-        let input = Input::new(6, 10, vec![1, 11, 30, 34, 35, 37]).unwrap();
+    fn test_2() {
+        let input = Input::new(
+            6.try_into().unwrap(),
+            10.try_into().unwrap(),
+            vec![1, 11, 30, 34, 35, 37],
+        )
+        .unwrap();
         assert_eq!(max_chicken_protected(input), 4);
+    }
+
+    #[test]
+    fn test_3() {
+        let input = Input::new(
+            6.try_into().unwrap(),
+            10.try_into().unwrap(),
+            vec![1, 2, 3, 4, 5, 6],
+        )
+        .unwrap();
+        assert_eq!(max_chicken_protected(input), 6);
+    }
+
+    #[test]
+    fn test_4() {
+        let input = Input::new(
+            6.try_into().unwrap(),
+            4.try_into().unwrap(),
+            vec![1, 2, 4, 5, 6, 8],
+        )
+        .unwrap();
+        assert_eq!(max_chicken_protected(input), 3);
     }
 }
