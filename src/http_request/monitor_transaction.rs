93f05b29ff2d238b21ec6a6d565adccd2f207b09
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2024-03-14 22:07:51 +0700

    implement monitor

--- src/http_request/monitor_transaction.rs
+++ src/http_request/monitor_transaction.rs
@@ -1,3 +1,42 @@
-pub fn monitor() {
-    todo!();
+const MONITOR_URL: &str = "https://mock-node-wgqbnxruha-as.a.run.app/check";
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+enum ServerTransactionStatus {
+    Confirmed,
+    Failed,
+    Pending,
+    Dne,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Response {
+    tx_status: ServerTransactionStatus,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    RequestErr(reqwest::Error),
+    InvalidResponseBody,
+    Failed,
+    Pending,
+    Dne,
+}
+
+pub type MonitorResult = Result<(), Error>;
+
+pub async fn monitor(tx_hash: &str) -> MonitorResult {
+    let url = format!("{}/{}", MONITOR_URL, tx_hash);
+    let client = reqwest::Client::new();
+    let response = client.get(url).send().await.map_err(Error::RequestErr)?; // map err and early return
+    let response_body = response.text().await.map_err(Error::RequestErr)?; // map err and early return
+    let response: Response =
+        serde_json::from_str(&response_body).map_err(|_| Error::InvalidResponseBody)?; // map err and early return
+    match response.tx_status {
+        ServerTransactionStatus::Confirmed => Ok(()),
+        // match negative parsed transaction status into errors to comply with the Rust's convention
+        ServerTransactionStatus::Failed => Err(Error::Failed),
+        ServerTransactionStatus::Pending => Err(Error::Pending),
+        ServerTransactionStatus::Dne => Err(Error::Dne),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,27 @@ mod supermans_chicken_rescue;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-
     // let result = boss_babys_revenge::check_boss_behavior("SRSSRRR".try_into().unwrap());
     // println!("result: {result:?}");
 
-    {
-        use http_request::broadcast_transaction::*;
-        let transaction = Transaction {
-            symbol: Symbol::try_from("ETC").unwrap(),
-            price: 555.try_into().unwrap(),
-            time_stamp: TimeStamp::now(),
-        };
-        let result = broadcast(&transaction).await;
-        println!("broadcast result: {result:?}");
-    }
+    // test_broadcast().await;
+    test_monitor().await;
+}
+
+async fn test_broadcast() {
+    use http_request::broadcast_transaction::*;
+    let transaction = Transaction {
+        symbol: Symbol::try_from("ETC").unwrap(),
+        price: 555.try_into().unwrap(),
+        time_stamp: TimeStamp::now(),
+    };
+    let result = broadcast(&transaction).await;
+    println!("broadcast result: {result:?}");
+}
+
+async fn test_monitor() {
+    use http_request::monitor_transaction::*;
+    let tx_hash = "abcdefg";
+    let result = monitor(tx_hash).await;
+    println!("monitor result: {result:?}");
 }
